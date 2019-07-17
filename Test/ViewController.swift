7aff5f5f50b371d05878f0f5dc2d4b7b76e3c6d4
AhmadSafdar <ahmad@penandweb.com>
2019-07-17 04:21:46 -0500

    asd

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -13,8 +13,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let userInput = "P<USAROGGER<<MICHAEL<<STEVEVE<<JR<<GEORGE<<<<<<<<<<<<<<<<<<<<<<YA11164461USA8502201M2001023<<<<<<<<<<<<<<00"
+        var fullName: String = "First Last"
+        let fullNameArr = userInput.components(separatedBy: "<<")
+        let fullNameArr12 = userInput.split(regex: "[<]+")
+        
+        var firstName: String = fullNameArr[0]
+        var lastName: String = fullNameArr[1]
+        
+        
+    }
+    
+    
+    func parseStr(str : String){
+        
     }
 
 
+}
+
+extension String {
+    
+    func split(regex pattern: String) -> [String] {
+        
+        do  {
+        let re = try? NSRegularExpression(pattern: pattern, options: [])
+        let nsString = self as NSString // needed for range compatibility
+        let stop = "<"
+        let modifiedString = re!.stringByReplacingMatches(
+            in: self,
+            options: [],
+            range: NSRange(location: 0, length: nsString.length),
+            withTemplate: stop)
+            return modifiedString.components(separatedBy: stop)
+        }
+        catch let error {
+            return []
+        }
+    }
+    
+    func matches(_ regex: String) -> Bool {
+        return self.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
+}
+
+
+extension NSRegularExpression {
+    convenience init(_ pattern: String) {
+        do {
+            try self.init(pattern: pattern)
+        } catch {
+            preconditionFailure("Illegal regular expression: \(pattern).")
+        }
+    }
 }
 
